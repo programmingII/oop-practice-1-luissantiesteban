9ce7219c86a35e6c7490c99f68b0fe25f250aa80
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-07 13:04:07 -0700

    Add files via upload

---
+++ exercise-java/exercise8.java
@@ -0,0 +1,14 @@
+/*Luis Santiesteban
+  07-02-2019
+  12:52*/
+
+ public class exercise8{ /*inicio de la clase*/
+ 
+ public static void main(String []args){ /*inicio del main*/
+  
+  System.out.println("   J    a    v     v    a   "); /*cadena de caracteres impresa en consola*/
+  System.out.println("   J   a a    v   v    a a  "); /*cadena de caracteres impresa en consola*/
+  System.out.println("J  J  aaaaa    v v    aaaaa "); /*cadena de caracteres impresa en consola*/
+  System.out.println(" JJ  a     a    v    a     a"); /*cadena de caracteres impresa en consola*/
+ }
+}
