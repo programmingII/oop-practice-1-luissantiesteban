8dac46eeaac129b4a39b54adec66dedc3945d73f
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-07 13:25:24 -0700

    Add files via upload

---
+++ exercise-java/exercise9.java
@@ -0,0 +1,11 @@
+/*Nombre: Luis Santiesteban
+  Dia: 07-02-2019
+  Hora: 1:07*/
+
+ public class exercise9{    /*inicio del main*/
+ public static void main(String[]args){ /*inicio del main*/
+ 
+ System.out.println("((25.5*3.5-3.5*3.5)/(40.5-4.5)) = "+ ((25.5*3.5-3.5*3.5)/(40.5-4.5))); /*se hace la operacion y se imprime el resultado*/
+ 
+ }
+}
