8e32e4e0aaa7b2a36fa04792ec0177cc480e00e9
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-07 13:35:54 -0700

    Update exercise9.java

--- exercise-java/exercise9.java
+++ exercise-java/exercise9.java
@@ -2,7 +2,7 @@
   Dia: 07-02-2019
   Hora: 1:07*/
 
- public class exercise9{    /*inicio del main*/
+ public class exercise9{    /*inicio de la clase*/
  public static void main(String[]args){ /*inicio del main*/
  
  System.out.println("((25.5*3.5-3.5*3.5)/(40.5-4.5)) = "+ ((25.5*3.5-3.5*3.5)/(40.5-4.5))); /*se hace la operacion y se imprime el resultado*/
