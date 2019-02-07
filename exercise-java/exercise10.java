debb74cb32612b7b11cbdd343320ca57f54276d8
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-07 13:39:44 -0700

    Add files via upload

---
+++ exercise-java/exercise10.java
@@ -0,0 +1,10 @@
+/*Nombre:Luis Santiesteban
+  Dia: 07-02-2019
+  Hora: 1:30 */
+ 
+ public class exercise10{ /*inicio de la clase*/
+ public static void main(String[] args) { /*inicio del main*/
+
+  System.out.println("4.0*(1-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)) = " + 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))); /*Realiza la operacion e imprime el resultado*/
+  }
+} 
