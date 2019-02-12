9c90608b28bc3ed9a7b2949a2db3b73c84ab225c
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-12 12:09:15 -0700

    Add files via upload

---
+++ exercise-java/exercise14.java
@@ -0,0 +1,26 @@
+/*Nombre: Luis Santiesteban
+  Dia: 12/02/2019
+  Hora:12:00 pm
+*/
+
+  public class exercise14{ /*start of the class*/
+  public static void main(String[]args){ /*start of the main*/
+
+ System.out.println("* * * * * * =================================="); /*console priting text*/
+ System.out.println(" * * * * *  =================================="); /*console priting text*/
+ System.out.println("* * * * * * =================================="); /*Console priting text*/
+ System.out.println(" * * * * *  =================================="); /*Console priting text*/
+ System.out.println("* * * * * * =================================="); /*console priting text*/
+ System.out.println(" * * * * *  =================================="); /*console priting text*/
+ System.out.println("* * * * * * =================================="); /*Console priting text*/
+ System.out.println(" * * * * *  =================================="); /*Console priting text*/
+ System.out.println("* * * * * * =================================="); /*console priting text*/
+ System.out.println("=============================================="); /*Console priting text*/
+ System.out.println("=============================================="); /*Console priting text*/
+ System.out.println("=============================================="); /*Console priting text*/
+ System.out.println("=============================================="); /*Console priting text*/
+ System.out.println("=============================================="); /*Console priting text*/
+ System.out.println("=============================================="); /*Console priting text*/
+ }
+
+}
---
+++ exercise-java/exercise15.java
@@ -0,0 +1,29 @@
+/*Nombre: Luis Santiesteban
+  Dia: 12/02/2019
+  Hora: 12:08 pm
+*/
+ 
+  public class exercise15{ /*start of the class*/
+  public static void main(String[]args){ /*start of the main*/
+  
+  int X1, X2, X3; /*Declaration of the variables*/
+  X1=23; X2=12; /*Initialization of the variables*/
+  
+  System.out.println(" BEFORE: X1= "+X1+ " X2 = "+X2); /*console priting result*/
+ 
+  X3=X1; /*Swapping*/
+  X1=X2; /*Swapping*/
+  X2=X3; /*Swapping*/
+
+ System.out.println("After: X1= " +X1+  " X2 = " +X2); /*Console priting result*/
+
+ }
+
+}
+ 
+  
+ 
+  
+
+
+  
