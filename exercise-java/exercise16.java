cac0ef400804d79a6f0cd395d94c540855568f4f
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-12 12:24:47 -0700

    Add files via upload

---
+++ exercise-java/exercise16.java
@@ -0,0 +1,16 @@
+/*Nombre: Luis Santiesteban
+  Dia: 12/02/2019
+  Hora: 12:14
+*/
+
+ public class exercise16{ /*start of the class*/ 
+ public static void main(String[]args){ /*start of the main*/
+ 
+ System.out.println(" +\"\"\"\"\"+ "); /*Console priting text*/
+ System.out.println("[| o o |]"); /*Console priting text*/
+ System.out.println(" |  ^  | "); /*Console priting text*/
+ System.out.println(" | '-' | "); /*Console priting text*/
+ System.out.println(" +-----+ "); /*Console priting text*/
+ }
+
+}
