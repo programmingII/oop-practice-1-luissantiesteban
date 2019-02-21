cc76dbe0baef3728f360efd6a9c735ebf38c9418
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-21 12:45:15 -0700

    Add files via upload

---
+++ exercise-java/exercise31.java
@@ -0,0 +1,15 @@
+// Nombre:Luis Santiesteban
+// Dia: 21/02/2019
+// Hora: 12:30
+
+public class exercise31 {
+public static void main(String[] args) {
+    System.out.println("\nJava Version: "+System.getProperty("java.version")); //Se obtiene la version de java con getProperty de la clase System 
+    System.out.println("Java Runtime Version: "+System.getProperty("java.runtime.version")); //Se obtiene la version runtime de java con getProperty de la clase System
+    System.out.println("Java Home: "+System.getProperty("java.home")); //Se obtiene home de java con getProperty de la clase System
+    System.out.println("Java Vendor: "+System.getProperty("java.vendor")); //Se obtiene el vendor de java con getProperty de la clase System
+    System.out.println("Java Vendor URL: "+System.getProperty("java.vendor.url")); //Se obtiene vendor URL de java con getProperty de la clase System
+    System.out.println("Java Class Path: "+System.getProperty("java.class.path")+"\n"); //Se obtiene la versionla class path de java con getProperty de la clase System
+
+    }
+}
