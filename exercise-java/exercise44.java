cf359292a9cc46862f2cc5d62ff81dcb6d2f8bf1
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-26 13:17:56 -0700

    Add files via upload

---
+++ exercise-java/exercise44.java
@@ -0,0 +1,19 @@
+//Nombre: Luis Santiesteban
+//Dia: 26/02/2019
+//Hora: 1:09 pm
+
+ import java.util.Scanner;//importacion de scanner
+
+ public class exercise44{//inicio de la clase
+ public static void main(String[] args){// inicio del main
+
+ Scanner in = new Scanner(System.in);//creacion del scanner
+
+	System.out.println("Input number");//mensaje en consola
+	int Num=in.nextInt();//lectura del numero
+
+	System.out.printf("%d + %d%d + %d%d%d ", Num,Num,Num,Num,Num,Num);//impresion
+	}
+
+}
+	
