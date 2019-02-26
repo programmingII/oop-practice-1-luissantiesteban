8f15aa4c8a35e584c2d9a55a45064c4adfde8499
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-26 13:01:38 -0700

    Add files via upload

---
+++ exercise-java/exercise43.java
@@ -0,0 +1,12 @@
+//Nombre: Luis Santiesteban
+//Dia: 26/02/2019
+//Hora: 12:58 pm
+ 
+ 
+ 
+ public class exercise43{ //Inicio de la clase
+ public static void main(String[]args){ //Inicio del main
+	System.out.println("\nTwinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!\n\n");//impresion del texto en varios renglones
+	}
+
+}
