a19009d7cc2a069d67c1347e60f6e849b2c8ef3e
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-27 13:54:48 -0700

    Add files via upload

---
+++ exercise-java/exercise48.java
@@ -0,0 +1,17 @@
+//Nombre: Luis Santiesteban
+//Dia: 27/02/2019
+//Hora: 1:48 pm
+
+ import java.util.Scanner;
+
+ public class exercise48{//inicio de la clase
+ public static void main(String[] args){// inicio del main
+
+	for(int i=1; i<=99; i++){
+	if(i%2!=0){
+	System.out.println(+i);
+	}
+	}
+ }
+
+}
