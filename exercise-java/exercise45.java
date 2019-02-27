bfeea43d6a66ecc2322816e3c454951b79109d2c
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-26 19:41:41 -0700

    Add files via upload

---
+++ exercise-java/exercise45.java
@@ -0,0 +1,14 @@
+//Nombre: Luis Santiesteban
+//Dia: 26/02/2019
+//Hora: 1:21 pm
+
+ 
+ import java.io.File; //importacion del file
+
+ public class exercise45{//inicio de la clase
+ public static void main(String[] args){////inicio del main
+ System.out.println("/Desktop/conexion.txt: "+("Luis.txt").length()+" bytes");//impresion de el peso del archivo en bytes
+
+	}
+
+}
