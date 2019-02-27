6396658a6d17720137e89fdda63644a6d4f26c01
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-27 13:12:37 -0700

    Add files via upload

---
+++ exercise-java/exercise46.java
@@ -0,0 +1,15 @@
+//Nombre: Luis Santiesteban
+//Dia: 27/02/2019
+//Hora: 12:55 pm
+
+import java.util.Date;//importacion de la libreria
+
+ public class exercise46{//inicio de la clase
+ public static void main(String[] args){// inicio del main
+
+	Date fecha = new Date(); //se crea el objeto fecha de tipo date
+ 	System.out.println("Fecha: "+fecha);//impresion de la fecha y hora
+
+	}
+
+}
