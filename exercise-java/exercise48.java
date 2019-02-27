d68b2e3e8b881b5f0fe77ff386f7dae4d5f3341f
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-27 13:56:41 -0700

    Update exercise48.java

--- exercise-java/exercise48.java
+++ exercise-java/exercise48.java
@@ -2,14 +2,12 @@
 //Dia: 27/02/2019
 //Hora: 1:48 pm
 
- import java.util.Scanner;
-
  public class exercise48{//inicio de la clase
  public static void main(String[] args){// inicio del main
 
-	for(int i=1; i<=99; i++){
-	if(i%2!=0){
-	System.out.println(+i);
+	for(int i=1; i<=99; i++){ // se repite el proceso hasta que i sea menor o igual a 99
+	if(i%2!=0){ //condicion para que se pueda imprimir el numero
+	System.out.println(+i);//impresion del numero en pantalla
 	}
 	}
  }
