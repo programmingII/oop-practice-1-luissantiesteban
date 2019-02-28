cfa9b07fa1ce840443e7a0ce9b598d6efcff7643
159907-LuisSantiesteban <46798802+159907-luissantiesteban@users.noreply.github.com>
2019-02-28 02:17:35 -0700

    Add files via upload

---
+++ exercise-java/exercise68.java
@@ -0,0 +1,22 @@
+//Nombre: Luis Santiesteban
+//Dia: 28/02/2019
+//Hora: 2:00
+
+ import java.lang.*;//importacion libreria lang
+
+ public class exercise68{//inicio de la clase
+ public static void main(String[] args){// inicio del main
+
+	String str = "Python 3.0";//variable con la cadena
+      
+   	String str1 = str.substring(str.length() - 3);//substring toma la subcadena con los ultimos 3 digitos y los guarda en otras variable
+   	System.out.println( str1+str1+str1+str1);//impresion
+  }
+    }
+
+
+
+	
+
+
+
